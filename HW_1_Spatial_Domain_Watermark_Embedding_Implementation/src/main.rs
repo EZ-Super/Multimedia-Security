8d70e7bc9a5f289976e034dcb6f17f8ac18353f0
EZ-Super <55626553+EZ-Super@users.noreply.github.com>
2025-03-13 01:05:53 +0800

    123

--- HW_1_Spatial_Domain_Watermark_Embedding_Implementation/src/main.rs
+++ HW_1_Spatial_Domain_Watermark_Embedding_Implementation/src/main.rs
@@ -1,3 +1,29 @@
+use image::{open, GenericImageView};
+use std::env;
+
 fn main() {
+    dotenv::dotenv().ok();
+    let path = env::var("elaine").expect("elaine read error");
+    match open(path){
+        Ok(image) => {
+            if image.color() == image::ColorType::Rgb8 {
+                let (width, height) = image.dimensions();
+                for x in 0..width{
+                    for y in 0..height{
+                        let [r,g,b,a] = image.get_pixel(x, y).0;
+                        println!("Pixel at ({}, {}): R: {}, G: {}, B: {}, A: {}", x, y, r, g, b, a);
+                    }
+                }
+            } else {
+                println!("Unsupported image color type {:?}", image.color());
+            }
+        },
+        Err(error) => {
+            println!("Error loading image: {}", error);
+        }
+    }
+
+
+
     println!("Hello, world!");
 }
